cc30956069d4c4846a9b55ad2d3d9a489fe9caef
patryk1991 <patryk1991@yahoo.com>
2013-11-03 17:21:37 -0500

    Update menus.cpp
    menu

--- menus.cpp
+++ menus.cpp
@@ -32,3 +32,63 @@ int safeInt()//people like to enter nonsense
      }while(cin.fail());//if the user enters a non integer, ask again
      return x;
 }
+
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int menu()
+{
+    
+    int a,b,c,d;
+ cout<<"Sorting Experiments\n---------------------\n"
+     "1. Data Managment\n"
+     "2. Test Run\n"
+     "3. Statistics\n"
+     "4. Quit\n";
+     
+    
+     cin>>a;
+     
+     
+
+       if(a ==1){ 
+        cout<<"Data Managment\n---------------------\n"
+     "1. Create New Data File\n"
+     "2. Add Data to file\n"
+     "3. Delete Data from File\n";
+     cin>>b;
+     }
+     
+     
+        if(a ==2){
+       
+       cout<<"Test Run\n---------------------\n"
+     "1. Bubble sort\n"
+     "2. Quick sort\n"
+     "3. Selection sort\n" ;
+      cin>>c;
+     
+     }
+     
+       if (a ==3){
+  
+        cout<<"Statisticsn\n---------------------\n"
+      
+        
+    
+       
+       }
+      
+      else if(a ==4){
+    cout << "you quit ";
+
+} 
+    
+system("pause");
+return 0;
+}
+
